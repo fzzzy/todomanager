6a88bf5ef6007ac25b4fe685f451c1e03661a027
Donovan Preston <46048+fzzzy@users.noreply.github.com>
2025-06-09 11:09:49 -0400

    Support both simple ajax and normal form posts

--- todosapp/views.py
+++ todosapp/views.py
@@ -1,24 +1,61 @@
 from django.shortcuts import get_object_or_404, render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 from .models import Todo
 
 
 def index(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
+        # Handle JSON POST data
+        if request.content_type == 'application/json':
+            try:
+                data = json.loads(request.body)
+                title = data.get('title')
+            except json.JSONDecodeError:
+                return JsonResponse({'error': 'Invalid JSON'}, status=400)
+        else:
+            # Handle form POST data
+            title = request.POST.get('title')
+        
         if title:
-            Todo.objects.create(title=title, pub_date=timezone.now())
+            todo = Todo.objects.create(title=title, pub_date=timezone.now())
+            # Return JSON if client accepts JSON
+            if request.headers.get('Accept') == 'application/json' or request.content_type == 'application/json':
+                return JsonResponse({
+                    'id': todo.id,
+                    'title': todo.title,
+                    'pub_date': todo.pub_date.isoformat()
+                }, status=201)
             return redirect('index')
     
     todos = Todo.objects.order_by("-pub_date")[:5]
+    
+    # Return JSON if client accepts JSON
+    if request.headers.get('Accept') == 'application/json':
+        todos_data = [{
+            'id': todo.id,
+            'title': todo.title,
+            'pub_date': todo.pub_date.isoformat()
+        } for todo in todos]
+        return JsonResponse({'todos': todos_data})
+    
     context = {"todos": todos}
     return render(request, "todosapp/index.html", context)
 
 
-
 def detail(request, todo_id):
     todo = get_object_or_404(Todo, pk=todo_id)
+    
+    # Return JSON if client accepts JSON
+    if request.headers.get('Accept') == 'application/json':
+        return JsonResponse({
+            'id': todo.id,
+            'title': todo.title,
+            'pub_date': todo.pub_date.isoformat()
+        })
+    
     return HttpResponse("You're looking at todo %s." % todo.id)
 
